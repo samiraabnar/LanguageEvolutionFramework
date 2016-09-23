52e44c40c166d7bd8eef7da22cbdcbf1d12d0fd3
Samira Abnar <samiraabnar@gmail.com>
2016-09-23 10:23:08 +0200

    Technically Working

--- src/RecurrentCommunicatingAgents.py
+++ src/RecurrentCommunicatingAgents.py
@@ -7,14 +7,14 @@ from scipy.spatial import distance
 import sys
 sys.path.append('../../')
 
-from LanguageEvolutionFramework.src.Vision.VGG16 import *
+from LanguageEvolutionFramework.src.Vision.VGG_16 import *
 
 class LSTM_Listener(object):
     def __init__(self,input_dim,output_dim,outer_output_dim):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.outer_output_dim = outer_output_dim
-        self.learning_rate = 0.1
+        self.learning_rate = 0.01
         self.random_state = np.random.RandomState(23455)
         self.initial_hiddens = [None,dict(initial=T.zeros(self.output_dim,dtype=theano.config.floatX)),
                           dict(initial=T.zeros(self.output_dim,dtype=theano.config.floatX))
@@ -94,13 +94,26 @@ class LSTM_Listener(object):
         
 
         self.O_w = theano.shared(value=O_w, name="O_w" , borrow="True")
-       
+
+        W_in = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (4096, self.outer_output_dim))
+            , dtype=theano.config.floatX)
+
+        W_out = np.asarray(self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (4096, self.outer_output_dim))
+            , dtype=theano.config.floatX)
+
+        self.W_in = theano.shared(value=W_in, name="W_in" , borrow="True")
+        self.W_out = theano.shared(value=W_out, name="W_out", borrow="True")
 
         self.params = [self.U_input, self.U_forget, self.U_output, self.W_input, self.W_forget, self.W_output,
-                        self.U, self.W
+                        self.U, self.W, self.O_w
                        ]
 
-        self.output_params = [self.O_w]
+        self.output_params = [self.W_out]
 
 
     def define_network(self):
@@ -116,13 +129,14 @@ class LSTM_Listener(object):
             forget_gate = T.nnet.hard_sigmoid(
                 T.dot((self.U_forget), x_t) + T.dot(self.W_forget, prev_state))
             output_gate = T.nnet.hard_sigmoid(
-                T.dot((self.U_output), x_t) + T.dot(self.W_output, prev_state) )
+                T.dot((self.U_output), x_t) + T.dot(self.W_output, prev_state))
 
             stabilized_input = T.tanh(T.dot((self.U), x_t) + T.dot(self.W, prev_state) )
             c = forget_gate * prev_content + input_gate * stabilized_input
             s = output_gate * T.tanh(c)
 
-            o = T.dot(self.O_w, s)
+
+            o = T.nnet.softmax(T.dot(self.O_w, s))[0]
 
             return [o, s, c, input_gate, forget_gate, output_gate]
 
@@ -135,68 +149,163 @@ class LSTM_Listener(object):
             truncate_gradient=-1,
             outputs_info=self.initial_hiddens)
 
-        self.predict = theano.function([X],[self.output[-1]])
+        output = self.output[-1] #T.nnet.softmax(T.dot(self.W_out,T.sum(self.output.T,axis=1)))[0]
+        self.predict = theano.function([X],[output])
 
-        params = self.params + self.output_params
-        cost = T.mean((self.output - Y) ** 2)
+        params = self.params #+ self.output_params
+        cost = T.sum(T.nnet.categorical_crossentropy(output,Y))# T.sum(distance.euclidean(output,Y))  #
         grads = T.grad(cost,params)
         updates = [(param_i, param_i - self.learning_rate * grad_i) for param_i, grad_i in zip(params, grads)]
-        self.backprop_update = theano.function([X,Y],[],updates=updates)
+        self.backprop_update = theano.function([X,Y],[cost],updates=updates)
+        p_updates = [(param_i, param_i + self.learning_rate * param_i + param_i*np.random.rand()) for param_i in params]
+        self.positive_reinforcement = theano.function([],[],updates=p_updates)
+        n_updates = [(param_i, param_i - self.learning_rate * param_i + param_i*np.random.rand()) for param_i in params]
+        self.negetive_reinforcement = theano.function([],[],updates=n_updates)
+
+
 
 
 class LSTM_Talker(object):
-    def __init__(self,dims):
-        self.dimensions = dims
-        self.number_of_layers = len(dims)
-        self.learning_rate = 0.1
+    def __init__(self, input_dim, output_dim, outer_output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.outer_output_dim = outer_output_dim
+        self.learning_rate = 0.03
+        self.random_state = np.random.RandomState(23455)
+        self.initial_hiddens = [None, dict(initial=T.zeros(self.output_dim, dtype=theano.config.floatX)),
+                                dict(initial=T.zeros(self.output_dim, dtype=theano.config.floatX))
+            , None, None, None
+                                ]
 
+    def init_lstm_weights(self):
+        U_input = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     (self.output_dim, self.input_dim))
+            , dtype=theano.config.floatX)
 
+        U_forget = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     (self.output_dim, self.input_dim))
+            , dtype=theano.config.floatX)
+
+        U_output = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     (self.output_dim, self.input_dim))
+            , dtype=theano.config.floatX)
+
+        W_input = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (self.output_dim, self.output_dim))
+            , dtype=theano.config.floatX)
+
+        W_forget = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (self.output_dim, self.output_dim))
+            , dtype=theano.config.floatX)
+
+        W_output = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (self.output_dim, self.output_dim))
+            , dtype=theano.config.floatX)
+
+        U = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.input_dim + self.output_dim)),
+                                     (self.output_dim, self.input_dim))
+            , dtype=theano.config.floatX)
+
+        W = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (self.output_dim, self.output_dim))
+            , dtype=theano.config.floatX)
+
+        self.W = theano.shared(value=W, name="W", borrow="True")
+        self.U = theano.shared(value=U, name="U", borrow="True")
+
+        self.W_input = theano.shared(value=W_input, name="W_input", borrow="True")
+        self.U_input = theano.shared(value=U_input, name="U_input", borrow="True")
+
+        self.W_output = theano.shared(value=W_output, name="W_output", borrow="True")
+        self.U_output = theano.shared(value=U_output, name="U_output", borrow="True")
+
+        self.W_forget = theano.shared(value=W_forget, name="W_forget", borrow="True")
+        self.U_forget = theano.shared(value=U_forget, name="U_forget", borrow="True")
+
+        O_w = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (self.outer_output_dim, self.output_dim))
+            , dtype=theano.config.floatX)
+
+        self.O_w = theano.shared(value=O_w, name="O_w", borrow="True")
+
+        W_in = np.asarray(
+            self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                     (4096, self.outer_output_dim))
+            , dtype=theano.config.floatX)
+
+        W_out = np.asarray(self.random_state.normal(-np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                                    np.sqrt(6.0 / (self.output_dim + self.output_dim)),
+                                                    (4096, self.outer_output_dim))
+                           , dtype=theano.config.floatX)
+
+        self.W_in = theano.shared(value=W_in, name="W_in", borrow="True")
+        self.W_out = theano.shared(value=W_out, name="W_out", borrow="True")
+
+        self.params = [self.U_input, self.U_forget, self.U_output, self.W_input, self.W_forget, self.W_output,
+                       self.U, self.W, self.W_out
+                       ]
+
+        self.output_params = [self.O_w]
 
     def define_network(self):
-
         X = T.matrix('input')
         Y = T.vector('output')
 
+        self.init_lstm_weights()
 
-        W = []
-        for i in np.arange(self.number_of_layers - 1):
-            w = self.init_weights((self.dimensions[i],self.dimensions[i+1]))
-            W.append(w)
+        def forward_step(x_t, prev_state, prev_content):
+            input_gate = T.nnet.hard_sigmoid(
+                T.dot((self.U_input), x_t) + T.dot(self.W_input, prev_state))
+            forget_gate = T.nnet.hard_sigmoid(
+                T.dot((self.U_forget), x_t) + T.dot(self.W_forget, prev_state))
+            output_gate = T.nnet.hard_sigmoid(
+                T.dot((self.U_output), x_t) + T.dot(self.W_output, prev_state))
 
+            stabilized_input = T.tanh(T.dot((self.U), x_t) + T.dot(self.W, prev_state))
+            c = forget_gate * prev_content + input_gate * stabilized_input
+            s = output_gate * T.tanh(c)
 
-        layers_activations = []
-        layers_activations.append(X)
-        for i in np.arange(1,self.number_of_layers):
-            activation = theano.shared(np.asarray(np.zeros(self.dimensions)))
-            layers_activations.append(activation)
+            o = T.dot(self.O_w, s)
 
-        for i in np.arange(1,self.number_of_layers):
-            layers_activations[i] = T.nnet.sigmoid(T.dot(layers_activations[i-1], W[i-1]))
+            return [o, s, c, input_gate, forget_gate, output_gate]
 
+        [self.output, self.hidden_state, self.memory_content, self.input_gate, self.forget_gate,
+         self.output_gate], updates = theano.scan(
+            forward_step,
+            sequences=[input],
+            truncate_gradient=-1,
+            outputs_info=self.initial_hiddens)
 
-        output = layers_activations[-1]
+        output = T.dot(self.W_out, self.output.T)
+        self.predict = theano.function([X], [output])
 
-        self.forward_step = theano.function([X],[output])
-
+        params = self.params + self.output_params
         cost = T.mean((output - Y) ** 2)
-        grads = T.grad(cost,W)
-        updates = [(param_i, param_i - self.learning_rate * grad_i) for param_i, grad_i in zip(W, grads)]
-        self.backprop_update = theano.function([X,Y],[],updates=updates)
+        grads = T.grad(cost, params)
+        updates = [(param_i, param_i - self.learning_rate * grad_i) for param_i, grad_i in zip(params, grads)]
+        self.backprop_update = theano.function([X, Y], [], updates=updates)
 
 
-    def reinforce_weights(self,W):
-        pass
-
-
-
-    def init_weights(self,shape):
-        """ Weight initialization """
-        weights = np.asarray(np.random.randn(*shape), dtype=theano.config.floatX)
-        return theano.shared(weights)
-
-
-
-Alpha = ['A', 'B'] #, 'C', 'D', 'E']
+Alpha = ['A', 'B', 'C', 'D', 'E']
 def get_string(list_of_vec):
     str = " "
     for i in np.arange(len(list_of_vec)):
@@ -204,12 +313,16 @@ def get_string(list_of_vec):
 
     return str
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
 if __name__ == '__main__':
     #rfnn_talker = AttendedLSTM(input_dim=5, output_dim=5, number_of_layers=1,
     #                               hidden_dims=[512], dropout_p=0.0, learning_rate=0.01) # ReinforcedFeedForwardNeuralNetwork_Talker([4096,512,2])
     #rfnn_talker.build_model()
 
-    lstm_listener = LSTM_Listener(input_dim=5, output_dim=1000, outer_output_dim=4096)#ReinforcedFeedForwardNeuralNetwork_Listener([2, 4096*2, 4096])
+    lstm_listener = LSTM_Listener(input_dim=5, output_dim=2000, outer_output_dim=4096)#ReinforcedFeedForwardNeuralNetwork_Listener([2, 4096*2, 4096])
     lstm_listener.define_network()
 
 
@@ -217,8 +330,11 @@ if __name__ == '__main__':
     vgg = VGG_16('vgg16_weights.h5')
     vgg.model.compile(optimizer='adam', loss='categorical_crossentropy')
 
+    d = np.zeros((1, 5), dtype='float32')
+    d[0][0] = 1.0
+    lstm_listener.predict(d)
 
-    for k in np.arange(5000):
+    for k in np.arange(100):
         i = np.random.randint(2)#len(images))
         j = np.random.randint(2)#len(images))
 
@@ -229,32 +345,50 @@ if __name__ == '__main__':
         while j == i:
             j = np.random.randint(2)#len(images))
 
-        rep1 = vgg.get_representation(images[i])
-        rep2 = vgg.get_representation(images[j])
+        rep1 = softmax(vgg.get_representation(images[i])[0])
+        rep2 = softmax(vgg.get_representation(images[j])[0])
 
 
 
-        d = np.zeros((1,5),dtype='float32')
-        d[0][i] = 1.0
+        d = np.zeros((2,5),dtype='float32')
+        d[0][i%2] = 1.0
+        d[1][(i+1)%2] = 1.0
 
 
         #description0 = rfnn_talker.forward_step(rep1)#[d]#
         #description1 = description0[0]
         #description = np.transpose(description1)
-        recons_rep = lstm_listener.predict(np.round(d))
-
+        recons_rep = lstm_listener.predict(d)
         dist1 = distance.euclidean(recons_rep,rep1)
         dist2 = distance.euclidean(recons_rep,rep2)
-
         selected = rep1
-        state = "succeed!"
+
+        state = "Failed!"
         if dist1 > dist2:
-            selected = rep2
-            state = "Failed!"
+            if dist2 < 1:
+                selected = rep2
+                state = "Wrong Choice!"
+        else:
+            if dist1 < 1:
+                state = "succeed!"
 
 
-        print(get_string(np.round(d))+" "+str(i))
+
+
+        """
+        print(np.eye(2)[i])
+        if np.argmax(recons_rep) == i:
+            state = "Succeed!"
+            [cost] = lstm_listener.backprop_update(d,np.asarray(np.eye(2)[i],dtype="float32"))
+        else:
+            state = "Faild!"
+            [cost] = lstm_listener.backprop_update(d,np.asarray(np.eye(2)[i],dtype="float32"))
+        """
+        [cost] = lstm_listener.backprop_update(d,rep1)
+        print(cost)
+        print(get_string(d)+" "+str(i))
         print(state)
         #rfnn_talker.backprop_update(selected,np.round(description0[0][0]))
-        lstm_listener.backprop_update(np.round(d),rep1[0])
+
+
 
---
+++ src/Vision/VGG_16.py
@@ -0,0 +1,187 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2, numpy as np
+import theano
+import os
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
+from sklearn import (manifold, datasets, decomposition, ensemble,
+                     discriminant_analysis, random_projection)
+from time import time
+
+
+class VGG_16(object):
+
+    def __init__(self,weights_path=None):
+        model = Sequential()
+        l1 = ZeroPadding2D((1,1),input_shape=(3,224,224))
+        model.add(l1)
+        l2 = Convolution2D(64, 3, 3, activation='relu')
+        model.add(l2)
+        l3 = ZeroPadding2D((1,1))
+        model.add(l3)
+        l4 = Convolution2D(64, 3, 3, activation='relu')
+        model.add(l4)
+        l5 = MaxPooling2D((2,2), strides=(2,2))
+        model.add(l5)
+
+        l6 = ZeroPadding2D((1,1))
+        model.add(l6)
+        l7 = Convolution2D(128, 3, 3, activation='relu')
+        model.add(l7)
+        l8 = ZeroPadding2D((1, 1))
+        model.add(l8)
+        l9 = Convolution2D(128, 3, 3, activation='relu')
+        model.add(l9)
+        l10 = MaxPooling2D((2,2), strides=(2,2))
+        model.add(l10)
+
+        l11 = ZeroPadding2D((1,1))
+        model.add(l11)
+        l12 = Convolution2D(256, 3, 3, activation='relu')
+        model.add(l12)
+        l13 = ZeroPadding2D((1,1))
+        model.add(l13)
+        l14 = Convolution2D(256, 3, 3, activation='relu')
+        model.add(l14)
+        l15 = ZeroPadding2D((1,1))
+        model.add(l15)
+        l16 = Convolution2D(256, 3, 3, activation='relu')
+        model.add(l16)
+        l17 = MaxPooling2D((2,2), strides=(2,2))
+        model.add(l17)
+
+        l18 = ZeroPadding2D((1,1))
+        model.add(l18)
+        l19 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l19)
+        l20 = ZeroPadding2D((1,1))
+        model.add(l20)
+        l21 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l21)
+        l22 = ZeroPadding2D((1,1))
+        model.add(l22)
+        l23 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l23)
+        l24 = MaxPooling2D((2,2), strides=(2,2))
+        model.add(l24)
+
+        l25 = ZeroPadding2D((1,1))
+        model.add(l25)
+        l26 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l26)
+        l27 = ZeroPadding2D((1,1))
+        model.add(l27)
+        l28 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l28)
+        l29 = ZeroPadding2D((1,1))
+        model.add(l29)
+        l30 = Convolution2D(512, 3, 3, activation='relu')
+        model.add(l30)
+        l31 = MaxPooling2D((2,2), strides=(2,2))
+        model.add(l31)
+
+        l32 = Flatten()
+        model.add(l32)
+        l33 = Dense(4096, activation='relu')
+        model.add(l33)
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1000, activation='softmax'))
+
+        self.get_representation = theano.function([l1.input], l33.output,
+                                          allow_input_downcast=True)
+
+        if weights_path:
+            model.load_weights(weights_path)
+
+        self.model = model
+
+def load_images_from_folder(folder):
+    images = []
+    thumb_images = []
+    for filename in os.listdir(folder):
+        img = cv2.resize(cv2.imread(os.path.join(folder,filename)), (224, 224)).astype(np.float32)
+        img[:, :, 0] -= 103.939
+        img[:, :, 1] -= 116.779
+        img[:, :, 2] -= 123.68
+        img = img.transpose((2, 0, 1))
+        img = np.expand_dims(img, axis=0)
+        if img is not None:
+            images.append(img)
+
+        img2 = cv2.resize(cv2.imread(os.path.join(folder, filename)), (20, 20)).astype(np.float32)
+        thumb_images.append(img2)
+
+
+    return images, thumb_images
+
+
+
+#----------------------------------------------------------------------
+# Scale and visualize the embedding vectors
+def plot_embedding(X, image_tags, title=None):
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+
+    plt.figure()
+    ax = plt.subplot(111)
+
+    if hasattr(offsetbox, 'AnnotationBbox'):
+        # only print thumbnails with matplotlib > 1.0
+        shown_images = np.array([[1., 1.]])  # just something big
+        for i in range(X.shape[0]):
+            dist = np.sum((X[i] - shown_images) ** 2, 1)
+            #if np.min(dist) < 4e-3:
+                # don't show points that are too close
+            #    continue
+            shown_images = np.r_[shown_images, [X[i]]]
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(image_tags[i], cmap=plt.cm.gray_r),
+                X[i])
+            ax.add_artist(imagebox)
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+
+
+if __name__ == "__main__":
+    im = cv2.resize(cv2.imread('cat.jpg'), (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+
+    # Test pretrained model
+    vgg = VGG_16('vgg16_weights.h5')
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    vgg.model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    out = vgg.model.predict(im)
+    print(len(vgg.get_representation(im)[0]))
+
+    """  images,thumb_images = load_images_from_folder("shapes")
+
+
+
+    representations = []
+    for image in images:
+        representations.append(vgg.get_representation(image)[0])
+        #print(vgg.get_representation(image)[0])
+
+
+    print(len(images))
+
+    print("Computing t-SNE embedding")
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    t0 = time()
+    X_tsne = tsne.fit_transform(representations)
+
+    plot_embedding(X_tsne, thumb_images,
+                   "t-SNE embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+
+    plt.show()"""
